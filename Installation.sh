e80fdcdded382270384120fdfe15317db7d63355
If cli is a gift from god, using arch makes us prophets † <62696671+TheCyberArcher@users.noreply.github.com>
2024-01-28 14:32:23 +0100

    Update Installation.sh

--- Installation.sh
+++ Installation.sh
@@ -53,17 +53,24 @@ git clone https://github.com/fthx/no-overview.git ~/.local/share/gnome-shell/ext
 
 gsettings set org.gnome.shell disable-user-extensions false
 
-yay -S librewolf-bin torbrowser-launcher thunderbird keepassxc webapp-manager extension-manager qbittorrent btop onlyoffice-bin visual-studio-code-bin virtualbox openrgb-bin corectrl ddcutil vlc corectrl discord signal-desktop
+yay -S librewolf-bin torbrowser-launcher thunderbird keepassxc webapp-manager extension-manager qbittorrent btop onlyoffice-bin visual-studio-code-bin virtualbox openrgb-bin corectrl ddcutil vlc corectrl discord signal-desktop steam lutris proton-ge-custom 
 
 yay -S flatpak
 
-flatpak install flathub io.github.jeffshee.Hidamari
+sudo pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
+mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error \
+lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
+sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
+ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 \
+lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
 
 yay –S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon
 
+flatpak install flathub io.github.jeffshee.Hidamari
+
 sudo touch /etc/polkit-1/rules.d/90-corectrl.rules
 
-sudo cat > /etc/polkit-1/rules.d/90-corectrl.rules << EOF 
+sudo cat << EOF > /etc/polkit-1/rules.d/90-corectrl.rules
 
 " polkit.addRule(function(action, subject) {
     if ((action.id == "org.corectrl.helper.init" ||
@@ -73,9 +80,8 @@ sudo cat > /etc/polkit-1/rules.d/90-corectrl.rules << EOF
         subject.isInGroup("your-user-group")) {
             return polkit.Result.YES;
     }
-});"
+});" EOF
 
-EOF
 
 sudo touch /etc/modules-load.d/i2c.conf
 sudo echo i2c_dev >> /etc/modules-load.d/i2c.conf
@@ -83,15 +89,6 @@ sudo echo i2c_dev >> /etc/modules-load.d/i2c.conf
 yay -S virtualbox
 
 sudo usermod -a -G vboxusers $USER
-
-yay –S steam lutris proton-ge-custom 
-
-sudo pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
-mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error \
-lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
-sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
-ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 \
-lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
 
 echo "--- Installation terminée"
 echo "--- Reboot dans 10 secondes"
